a60227e86efc5e6c10c23fdd194cdf0948e4af9f
joel-b-john <joelbjohn737@gmail.com>
2024-09-27 23:10:33 +0530

    css updated for home text

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,11 +8,10 @@ const Home = () => {
       <div className="rectangle">
         <div className="text-container">
         <div className="greeting-container">
-          <h2 className="greeting-text"> Hello MY self ,</h2>
-          <h2 className="greeting-text"> Joel B John,</h2>
+          <h2 className="greeting-text"> Hello.,</h2>
+          <h2 className="greeting-text">I'm Joel B John,</h2>
 
-          <h1 className="animated-sentence">I'm<h1> <span className="animated-text">Full Stack Developer</span></h1>
-          </h1>
+          <div className="animated-sentence"><h5>A passionate </h5><h1> <span className="animated-text">Full Stack Developer</span></h1></div>
         </div>
         </div>
         <img src={yourImage} alt="Description" className="image" />
